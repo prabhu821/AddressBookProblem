4f2ddbf4def921f2f270f024d442feec98010c0f
rishiraj007 <rishiraj@mounttalent.com>
2022-12-07 18:08:54 +0530

    [Prabhu]: Add Contacts

---
+++ AddressBookProblem/AddressBookMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    internal class AddressBookMain
+    {
+        public static List<Contact> AddressBook = new List<Contact>();
+
+        public void AddContact()
+
+        {
+            Console.WriteLine("Enter how many contacts you want to add");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Contact contacts = new Contact();
+
+                Console.Write("Enter the First Name : ");
+                contacts.FName = Console.ReadLine();
+
+                Console.Write("Enter the last Name  : ");
+                contacts.LName = Console.ReadLine();
+
+                Console.Write("Enter the address  : ");
+                contacts.Address = Console.ReadLine();
+
+                Console.Write("Enter the City  : ");
+                contacts.City = Console.ReadLine();
+
+                Console.WriteLine("Enter the state : ");
+                contacts.State = Console.ReadLine();
+
+                while (true)
+                {
+                    Console.Write("Enter Zip Code  : ");
+                    string code = Console.ReadLine();
+
+                    if (code.Length == 6)
+                    {
+                        contacts.Zip = code;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Enter Your Phone Number: ");
+                    string number = Console.ReadLine();
+
+                    if (number.Length == 10)
+                    {
+                        contacts.PhoneNumber = number;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid 10 digit Phone Number.");
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Enter Your Email Address: ");
+                    string mail = Console.ReadLine();
+
+                    if (mail.Contains("@"))
+                    {
+                        contacts.Email = mail;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid Email Address.");
+                    }
+                }
+
+                AddressBook.Add(contacts);
+            }
+        }
+
+        public void ViewDetail()
+        {
+            if (AddressBook.Count > 0)
+            {
+                foreach (var item in AddressBook)
+                {
+                    PrintDetail(item);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Address book is empty! ");
+
+            }
+        }
+
+        public void PrintDetail(Contact item)
+        {
+            Console.WriteLine("First Name :   " + item.FName);
+            Console.WriteLine("Last Name :    " + item.LName);
+            Console.WriteLine("Address :      " + item.Address);
+            Console.WriteLine("City    :      " + item.City);
+            Console.WriteLine("State   :      " + item.State);
+            Console.WriteLine("Zip     :      " + item.Zip);
+            Console.WriteLine("Phone Number  : " + item.PhoneNumber);
+            Console.WriteLine("email  :       " + item.Email);
+        }
+    }
+}
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -4,8 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
             Contact contact = new Contact();
+            AddressBookMain addressBookMain = new AddressBookMain();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to AddressBook");
+                Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Exit");
+                Console.WriteLine("\nEnter option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressBookMain.AddContact();
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        addressBookMain.ViewDetail();
+                        Console.WriteLine("\n");
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
