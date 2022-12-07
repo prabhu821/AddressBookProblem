cf3017a9a841094bf52cd495921ed01105922d81
rishiraj007 <rishiraj@mounttalent.com>
2022-12-07 17:40:17 +0530

    [Prabhu]: Add Contact

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to AddressBook");
+            Contact contact = new Contact();
         }
     }
 }
