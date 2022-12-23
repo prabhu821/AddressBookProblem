7b299d346b2a34e34f7e5258dd69ba5172683a1a
rishiraj007 <rishiraj@mounttalent.com>
2022-12-23 20:25:18 +0530

    [Prabhu]: Add Multiple Address Book

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AddressBookProblem
+﻿using System;
+
+namespace AddressBookProblem
 {
     internal class Program
     {
@@ -6,11 +8,12 @@
         {
             Contact contact = new Contact();
             AddressBookMain addressBookMain = new AddressBookMain();
+            int count = 0;
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to AddressBook");
-                Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Edit Contact \n4.Delete Contact \n5.Exit");
+                Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Edit Contact \n4.Delete Contact \n5.Add Multiple Address \n6.Exit");
                 Console.WriteLine("\nEnter option to execute");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -29,6 +32,21 @@
                         break;
                     case 4:
                         addressBookMain.DeleteContact();
+                        break;
+                    case 5:
+                        Console.WriteLine("\nHow many Address Book you want to create ?");
+                        int addCount = Convert.ToInt32(Console.ReadLine());
+
+                        Dictionary<int, string> dictionary = new Dictionary<int, string>();
+                
+                        for (int i = 1; i <= addCount; i++)
+                        {
+                            count++;
+                            List<AddressBookMain> newAdd = new List<AddressBookMain>();
+                            dictionary.Add(i, "New Dictionary");
+                        }
+                        Console.WriteLine("Address Book Created "+count);
+                        addressBookMain.AddContact();
                         break;
                     default:
                         flag = false;
