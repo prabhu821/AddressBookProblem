3c96027cc97b7dc90c4efe0d2e7483683d19ecff
rishiraj007 <rishiraj@mounttalent.com>
2023-01-08 16:09:47 +0530

    [Prabhu] Add: Sort Contacts By Name

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -14,7 +14,7 @@ namespace AddressBookProblem
             {
                 Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Edit Contact " +
                     "\n4.Delete Contact \n5.Add Multiple Address \n6.Check for Duplicate Contact \n7.Search Person by City or State" +
-                    "\n8.View Person by City or State \n9.Count Person \n10.Exit");
+                    "\n8.View Person by City or State \n9.Count Person \n10.Order By Name \n11.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -44,6 +44,9 @@ namespace AddressBookProblem
                         break;
                     case 9:
                         addressBookMain.CountPerson();
+                        break;
+                    case 10:
+                        addressBookMain.SortContactByName();
                         break;
                     default:
                         flag = false;
